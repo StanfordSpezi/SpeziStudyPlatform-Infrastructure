8524b2632c0ab5c1e64827b2e3d8271a54114753
Philipp Nagy <philipp.nagy@tum.de>
2026-04-21 16:55:21 -0700

    reduce node count

--- terraform/gke.tf
+++ terraform/gke.tf
@@ -52,7 +52,7 @@ resource "google_container_node_pool" "primary" {
   location = var.zone
   cluster  = google_container_cluster.primary.name
 
-  node_count = 2
+  node_count = 1
 
   node_config {
     machine_type = "e2-standard-4"
